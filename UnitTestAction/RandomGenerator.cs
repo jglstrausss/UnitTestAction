71780c687f24252ac86a922cb17145b22984904b
Jack Strauss <jstrauss@singular.co.za>
2023-10-30 17:02:08 +0200

    Build Initial class with test

---
+++ UnitTestAction/RandomGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace UnitTestAction
+{
+  using System;
+
+  public static class RandomGenerator
+  {
+    public static int GenerateRandomNumber(int min, int max)
+    {
+      var random = new Random();
+      return random.Next(min, max);
+    }
+  }
+}
---
+++ UnitTestActionTests/RandomGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿namespace UnitTestActionTests
+{
+  using UnitTestAction;
+
+
+  public class RandomGeneratorTests
+  {
+    [Fact]
+    public void GenerateRandomNumber_ReturnNumberWithinSpecifiedRange()
+    {
+      // Arrange
+      var min = 1;
+      var max = 10;
+
+      // Act
+      var result = RandomGenerator.GenerateRandomNumber(min, max);
+
+      // Assert
+      Assert.InRange(result, min, max);
+    }
+  }
+}
